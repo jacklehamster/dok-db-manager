fb03cf99f765eed0073007c162b8b4ae8158c8df
Jack Le Hamster <jacklehamster@gmail.com>
2024-10-24 23:15:48 -0700

    ℹ️ Small change in put-data route handling string data.
    - Changed handling of string data in put-data route
    - Improved code clarity by using split and map functions to handle string data more effectively.
    
    Hmm, it looks like the developer is really splitting hairs with this code. Maybe they're just trying to avoid any "split" ends 🤣. Keep up the good work, coder!
    
    Files:
    M  src/node/routes/put-data.ts
    
    Committed using auto-commit: Thu Oct 24 2024 23:15:48 GMT-0700 (Pacific Daylight Time)
    Autocommit project: https://www.npmjs.com/package/@dobuki/autocommit
    All the lame jokes were generated by AI.

--- src/node/routes/put-data.ts
+++ src/node/routes/put-data.ts
@@ -33,7 +33,7 @@ export function addPutDataRoute(app: express.Express, { githubApi, auth }: Props
       } else if (typeof data[key] === "object") {
         data[key] = cleanData(data[key]);
       } else if (typeof data[key] === "string") {
-        data[key] = data[key].split("/").map((a, index) => index === 0 ? a : encodeURIComponent(a)).join("/");
+        data[key] = data[key].split("/").map((a) => a.split(" ").map(encodeURIComponent).join(" ")).join("/");
       }
     }
     return data;
